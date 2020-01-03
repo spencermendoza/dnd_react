d6df93f7b4219ebde2058ab71b5685c5e314d97c
nilz3ro <austin.j.buddington@gmail.com>
2020-01-03 01:44:39 -0500

    Breaking up components.

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import PlayerCardList from '../Player/PlayerCardList';
+import PlayerFormDialog from '../Player/PlayerFormDialog';
 import { playersExample } from '../Player/playerHelpers';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 import './App.css';
 
+// TODO: Put the "current" player in Context?
 class App extends Component {
+  state = {
+    players: playersExample,
+    dialogOpen: false
+  };
+
+  handleAddClick = () => {};
+  handleEditClick = () => {};
+  handleDialogConfirmClick = () => {};
+  handleDialogCancelClick = () => {};
+
   render() {
     return (
-      <div>
-        <PlayerCardList players={playersExample} />
-      </div>
+      <>
+        <PlayerCardList players={this.state.players} onEditClick={this.handleEditClick} />
+        <Button onClick={this.handleAddClick}>Add New Player</Button>
+        {this.state.dialogOpen ? (
+          <PlayerFormDialog
+            open
+            player={this.state.currentPlayer}
+            onClose={this.handleDialogClose}
+            onConfirm={this.handleDialogConfirm}
+          />
+        ) : null}
+      </>
     );
   }
 }
--- src/Player/PlayerCardList.js
+++ src/Player/PlayerCardList.js
@@ -1,65 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Box from '@material-ui/core/Box';
-import { Player } from './Player';
 import PlayerCard from './PlayerCard';
-import PlayerFormDialog from './PlayerFormDialog';
-import Button from '@material-ui/core/Button';
-import { updatePlayer } from './playerHelpers';
 
-class PlayerCardList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dialogOpen: false,
-      currentPlayer: Player.create(),
-      players: props.players || []
-    };
-  }
-
-  handleAddClick = () => {
-    this.setState({ currentPlayer: Player.create(), dialogOpen: true });
-  };
-
-  handleEditClick = player => {
-    this.setState({ currentPlayer: player, dialogOpen: true });
-  };
-
-  handleDialogClose = () => {
-    this.setState({ dialogOpen: false, currentPlayer: Player.create() });
-  };
-
-  handleDialogConfirm = player => {
-    const updatedPlayers = updatePlayer(this.state.players, player);
-    this.setState({
-      players: updatedPlayers,
-      dialogOpen: false,
-      currentPlayer: Player.create()
-    });
-    // TODO: Use sort / filter functions here.
-  };
-
-  render() {
-    return (
-      <>
-        <Box display="flex" flexDirection="column" justifyContent="center">
-          {this.state.players.map(player => (
-            <PlayerCard player={player} key={player.id} onEditClick={this.handleEditClick} />
-          ))}
-        </Box>
-        {/* TODO: Remove the Button and the PlayerFormDialog for better separation of concern. */}
-        <Box>
-          <Button onClick={this.handleAddClick}>Add New Player</Button>
-        </Box>
-        <PlayerFormDialog
-          player={this.state.currentPlayer}
-          open={this.state.dialogOpen}
-          onClose={this.handleDialogClose}
-          onConfirm={this.handleDialogConfirm}
-        />
-      </>
-    );
-  }
-}
+export const PlayerCardList = ({ players = [], onEditClick }) => {
+  return (
+    <Box display="flex" flexDirection="column" justifyContent="center">
+      {players.map(player => (
+        <PlayerCard player={player} key={player.id} onEditClick={onEditClick} />
+      ))}
+    </Box>
+  );
+};
 
 export default PlayerCardList;
